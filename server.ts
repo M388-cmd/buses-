7fa8fcc476cd03278d2ea76a9d2bf03f43b4d4c7
Tu Nombre <tuemail@example.com>
2026-05-01 00:20:17 +0200

    Configurar para despliegue en Render

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 async function startServer() {
   const app = express();
   const httpServer = http.createServer(app);
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   // Middleware to parse JSON
   app.use(express.json());
